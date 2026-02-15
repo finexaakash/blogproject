5380cff3b8cb942fbc493f971231c031b2e1afd4
finexaakash <aakashsaini0998@gmail.com>
2026-02-15 13:27:00 +0530

    first commit

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,14 +1,27 @@
 const conf = {
-  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL || ""),
-  appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID || ""),
-  appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID || ""),
-  appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID || ""),
-  appwriteLikesCollectionId: String(import.meta.env.VITE_APPWRITE_LIKES_COLLECTION_ID),
-  appwriteCommentsCollectionId: "comments",
-  appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID || ""),
-  cloudinaryCloudName: String(import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || ""),
-  cloudinaryUploadPreset: String(import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET || ""),
-  appwriteCommentLikesCollectionId: (import.meta.env.VITE_APPWRITE_COMMENTLIKES_COLLECTION_ID || ""),
-  appwriteFollowsCollectionId: (import.meta.env.VITE_APPWRITE_FOLLOWS_COLLECTION_ID || ""),  
+  appwriteUrl: import.meta.env.VITE_APPWRITE_URL,
+  appwriteProjectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  appwriteDatabaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+
+  // Posts
+  appwriteCollectionId: import.meta.env.VITE_APPWRITE_COLLECTION_ID,
+
+  // Storage
+  appwriteBucketId: import.meta.env.VITE_APPWRITE_BUCKET_ID,
+
+  // Likes on Post
+  appwriteLikesCollectionId: import.meta.env.VITE_APPWRITE_LIKES_COLLECTION_ID,
+
+  // Comments
+  appwriteCommentsCollectionId: import.meta.env.VITE_APPWRITE_COMMENTS_COLLECTION_ID,
+
+  // Comment Likes
+  appwriteCommentLikesCollectionId:
+    import.meta.env.VITE_APPWRITE_COMMENTLIKES_COLLECTION_ID,
+
+  // Follow System
+  appwriteFollowsCollectionId:
+    import.meta.env.VITE_APPWRITE_FOLLOWS_COLLECTION_ID,
 };
+
 export default conf;
